bab832bf29aba545c952273b3157b4f36f65ebac
Andres Mendoza <andres.david.mm@hotmail.com>
2023-05-08 22:08:44 -0500

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const User = sequelize.define('user', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    // countryId
+});
+
+module.exports = User;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const userRouter = require('./user.router');
+const countryRouter = require('./country.router');
+const cityRouter = require('./city.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/users', userRouter)
+router.use('/countries', countryRouter)
+router.use('/cities', cityRouter);
 
 module.exports = router;
